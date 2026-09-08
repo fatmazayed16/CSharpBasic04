43d1be2e2d5be1ed4caddbbab49cd6f86f24d2f3
فاطمه محمود السيد عبدالرحيم زايد <fatma30982329@f-eng.tanta.edu.eg>
2026-09-08 23:41:59 +0300

    2nd Answer

--- C#Basic04/Program.cs
+++ C#Basic04/Program.cs
@@ -4,11 +4,22 @@
     {
         static void Main(string[] args)
         {
+            #region 1st Answer
             string title = "Clean Code";
             string upperTitle = title.ToUpper();
 
             Console.WriteLine(title);
-            Console.WriteLine(upperTitle);  
+            Console.WriteLine(upperTitle);
+            #endregion
+
+            #region 2nd Answer
+            string title2 = "Clean Code";
+            string title02 = "Clean Code";
+            Console.WriteLine(ReferenceEquals(title02, title2));
+            // Output: True 
+            // Explanation: In C#, string literals are interned, meaning that identical string literals are stored only once in memory. 
+            //Therefore, both title2 and title02 refer to the same memory location, resulting in ReferenceEquals returning true.
+            #endregion
 
         }
     }
